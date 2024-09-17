ff6548428b58e593b38009d1eadb22ed811e00c0
Youssef <youssefkh.tv2@gmail.com>
2024-09-17 17:17:16 +0100

    first commit

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "heroStyle.css"
+import logo from ''
+export const hero = () => {
+  return (
+    <div className='navbar'>
+        <img src="" />
+        <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <a className='contact_btn'>Contact With Me</a>
+        
+    </div>
+  )
+}
