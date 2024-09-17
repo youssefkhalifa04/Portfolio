11911ceca883e1c593add4e9067b678d32f19687
Youssef <youssefkh.tv2@gmail.com>
2024-09-17 20:17:14 +0100

    I've created the hero component and styling it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import { Navbar } from "./components/navbar/navbar"
-
+import { Hero } from "./components/hero/hero"
 function App() {
   
 
   return (
     <>
       <Navbar/>
-      
+      <Hero/>
     </>
   )
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
-export const hero = () => {
+import pic from '../../assets/avatar.jpeg'
+import './HeroStyle.css'
+export const Hero = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <img src={pic} className='hero-img'/>
+      <h1><span>I'm youssef Khalifa</span>, fullstack developer based in Tunisia.</h1>
+      <p>I am a fullstack developer from Mounastir,Tunisia.</p>
+      <div className="btn-container">
+        
+        <button className='connect'>
+          Connect with me
+        </button>
+        <button className='cv'>
+           My resume
+        </button>
+      </div>
     </div>
   )
 }
