1e9209b581d740a54048bdd34b24af9ce83ddbf9
Youssef <youssefkh.tv2@gmail.com>
2024-09-17 20:39:25 +0100

    I've added some options in the navigation menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo.svg'
 import './NavbarStyle.css'
 export const Navbar = () => {
+  const [menu , setMenu] = useState("home");
   return (
     <div className='navbar'>
         <div className="logo">Youssef</div>
         <ul className='nav-menu'>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
+            <li onClick={()=>{setMenu("home")}}>Home {menu=="home"?<hr />:<></> }</li>
+            <li onClick={()=>{setMenu("about")}}>About Me{menu=="about"?<hr />:<></> }</li>
+            <li onClick={()=>{setMenu("services")}}>Services{menu=="services"?<hr />:<></> }</li>
+            <li onClick={()=>{setMenu("portfolio")}}>Portfolio{menu=="portfolio"?<hr />:<></> }</li>
+            <li onClick={()=>{setMenu("contact")}}>Contact{menu=="contact"?<hr />:<></> }</li>
         </ul>
         <div className="contact-btn">Contact With Me</div>
         
